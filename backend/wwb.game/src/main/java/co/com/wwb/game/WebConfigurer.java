25398ead38ae8b38c80b77467e2ec5ce1b415eeb
Carlos Zuluaga <carlos.zuluaga@gmail.com>
2021-08-30 21:28:51 -0500

    Cambios para habilitar cabeceras para cors

--- backend/wwb.game/src/main/java/co/com/wwb/game/WebConfigurer.java
+++ backend/wwb.game/src/main/java/co/com/wwb/game/WebConfigurer.java
@@ -12,7 +12,7 @@ public class WebConfigurer implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/*")
-				.allowedOrigins("*")
+				.allowedOrigins("http://localhost:4200")
 				.allowedMethods("*")
 				.allowedHeaders("*")
 				.allowCredentials(true);
